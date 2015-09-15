bc7da80f4f4e7d4cc2b850f549fa0569098c7990
Kris Hicks <khicks@pivotal.io>
2015-09-14 17:12:57 -0700

    Extract Consul Lock methods from runtime-schema
    [#102329896]
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

---
+++ locket.go
@@ -0,0 +1,72 @@
+package locket
+
+import (
+	"path"
+	"time"
+
+	"github.com/cloudfoundry-incubator/consuladapter"
+	"github.com/cloudfoundry-incubator/locket/maintainer"
+	"github.com/cloudfoundry-incubator/runtime-schema/models"
+	"github.com/pivotal-golang/clock"
+	"github.com/pivotal-golang/lager"
+	"github.com/tedsuo/ifrit"
+)
+
+const LockSchemaRoot = "v1/locks"
+
+func LockSchemaPath(lockName string) string {
+	return path.Join(LockSchemaRoot, lockName)
+}
+
+const LockTTL = 10 * time.Second
+const RetryInterval = 5 * time.Second
+
+type Locket struct {
+	consul *consuladapter.Session
+	logger lager.Logger
+	clock  clock.Clock
+}
+
+func New(consul *consuladapter.Session, clock clock.Clock, logger lager.Logger) *Locket {
+	return &Locket{
+		consul: consul,
+		logger: logger,
+		clock:  clock,
+	}
+}
+
+func (l *Locket) NewAuctioneerLock(auctioneerPresence models.AuctioneerPresence, retryInterval time.Duration) (ifrit.Runner, error) {
+	auctionerPresenceJSON, err := models.ToJSON(auctioneerPresence)
+	if err != nil {
+		return nil, err
+	}
+	return maintainer.NewLock(l.consul, LockSchemaPath("auctioneer_lock"), auctionerPresenceJSON, l.clock, retryInterval, l.logger), nil
+}
+
+func (l *Locket) NewConvergeLock(convergerID string, retryInterval time.Duration) ifrit.Runner {
+	return maintainer.NewLock(l.consul, LockSchemaPath("converge_lock"), []byte(convergerID), l.clock, retryInterval, l.logger)
+}
+
+func (l *Locket) NewNsyncBulkerLock(bulkerID string, retryInterval time.Duration) ifrit.Runner {
+	return maintainer.NewLock(l.consul, LockSchemaPath("nsync_bulker_lock"), []byte(bulkerID), l.clock, retryInterval, l.logger)
+}
+
+func (l *Locket) NewRouteEmitterLock(emitterID string, retryInterval time.Duration) ifrit.Runner {
+	return maintainer.NewLock(l.consul, LockSchemaPath("route_emitter_lock"), []byte(emitterID), l.clock, retryInterval, l.logger)
+}
+
+func (l *Locket) NewRuntimeMetricsLock(runtimeMetricsID string, retryInterval time.Duration) ifrit.Runner {
+	return maintainer.NewLock(l.consul, LockSchemaPath("runtime_metrics_lock"), []byte(runtimeMetricsID), l.clock, retryInterval, l.logger)
+}
+
+func (l *Locket) NewTpsWatcherLock(tpsWatcherID string, retryInterval time.Duration) ifrit.Runner {
+	return maintainer.NewLock(l.consul, LockSchemaPath("tps_watcher_lock"), []byte(tpsWatcherID), l.clock, retryInterval, l.logger)
+}
+
+func (l *Locket) NewBBSMasterLock(bbsPresence models.BBSPresence, retryInterval time.Duration) (ifrit.Runner, error) {
+	bbsPresenceJSON, err := models.ToJSON(bbsPresence)
+	if err != nil {
+		return nil, err
+	}
+	return maintainer.NewLock(l.consul, LockSchemaPath("bbs_lock"), bbsPresenceJSON, l.clock, retryInterval, l.logger), nil
+}
