6f42eada409e9399bbd88696b7b9facc228503fd
James Myers <jfmyers9@gmail.com>
2017-04-17 15:33:10 -0700

    add NewPresenceRunner that retries on lost lock
    * NewLockRunner still exits after lost lock
    
    [#142867199]
    
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

--- lock/lock.go
+++ lock/lock.go
@@ -13,11 +13,12 @@ import (
 type lockRunner struct {
 	logger lager.Logger
 
-	locker        models.LocketClient
-	lock          *models.Resource
-	ttlInSeconds  int64
-	clock         clock.Clock
-	retryInterval time.Duration
+	locker         models.LocketClient
+	lock           *models.Resource
+	ttlInSeconds   int64
+	clock          clock.Clock
+	retryInterval  time.Duration
+	exitOnLostLock bool
 }
 
 func NewLockRunner(
@@ -29,12 +30,32 @@ func NewLockRunner(
 	retryInterval time.Duration,
 ) *lockRunner {
 	return &lockRunner{
-		logger:        logger,
-		locker:        locker,
-		lock:          lock,
-		ttlInSeconds:  ttlInSeconds,
-		clock:         clock,
-		retryInterval: retryInterval,
+		logger:         logger,
+		locker:         locker,
+		lock:           lock,
+		ttlInSeconds:   ttlInSeconds,
+		clock:          clock,
+		retryInterval:  retryInterval,
+		exitOnLostLock: true,
+	}
+}
+
+func NewPresenceRunner(
+	logger lager.Logger,
+	locker models.LocketClient,
+	lock *models.Resource,
+	ttlInSeconds int64,
+	clock clock.Clock,
+	retryInterval time.Duration,
+) *lockRunner {
+	return &lockRunner{
+		logger:         logger,
+		locker:         locker,
+		lock:           lock,
+		ttlInSeconds:   ttlInSeconds,
+		clock:          clock,
+		retryInterval:  retryInterval,
+		exitOnLostLock: false,
 	}
 }
 
@@ -44,7 +65,7 @@ func (l *lockRunner) Run(signals <-chan os.Signal, ready chan<- struct{}) error 
 	logger.Info("started")
 	defer logger.Info("completed")
 
-	var acquired bool
+	var acquired, isReady bool
 	_, err := l.locker.Lock(context.Background(), &models.LockRequest{Resource: l.lock, TtlInSeconds: l.ttlInSeconds})
 	if err != nil {
 		logger.Error("failed-to-acquire-lock", err)
@@ -52,6 +73,7 @@ func (l *lockRunner) Run(signals <-chan os.Signal, ready chan<- struct{}) error 
 		logger.Info("acquired-lock")
 		close(ready)
 		acquired = true
+		isReady = true
 	}
 
 	retry := l.clock.NewTimer(l.retryInterval)
@@ -75,11 +97,18 @@ func (l *lockRunner) Run(signals <-chan os.Signal, ready chan<- struct{}) error 
 			if err != nil {
 				if acquired {
 					logger.Error("lost-lock", err)
-					return err
+					if l.exitOnLostLock {
+						return err
+					}
+
+					acquired = false
 				}
 			} else if !acquired {
 				logger.Info("acquired-lock")
-				close(ready)
+				if !isReady {
+					close(ready)
+					isReady = true
+				}
 				acquired = true
 			}
 
