aea6ad3c192f1a9d1e6590f71964e3d5813c6a9a
Brandon Roberson <brandon.roberson@broadcom.com>
2025-02-14 16:14:35 +0000

    Change github.com/golang/protobuf (deprecated) to google.golang.org/protobuf

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,7 +1,7 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd "$DIR/../models"
-protoc --proto_path=../../vendor:../../vendor/github.com/golang/protobuf/ptypes/duration/:. \
+protoc --proto_path=../../vendor:../../vendor/google.golang.org/protobuf/types/known/durationpb/:. \
     --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     ./*.proto
