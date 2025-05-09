6a31f4e99097efb7e8c21551e4452c33c43dea24
Brandon Roberson <brandon.roberson@broadcom.com>
2025-05-09 20:26:38 +0000

    Remove debug flag

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd "$DIR/../models"
 protoc --proto_path=../../bbs/protoc-gen-go-bbs:../../vendor:../../vendor/google.golang.org/protobuf/types/known/durationpb/:. \
     --go_out=. --go_opt=paths=source_relative \
-    --go-bbs_out=. --go-bbs_opt=paths=source_relative,debug=true \
+    --go-bbs_out=. --go-bbs_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     ./*.proto 
 popd
